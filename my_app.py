5fe9e99b98e642c489fb974664abe5b4c7e001ce
TomMonks <thomas.m.monks@gmail.com>
2023-08-02 17:17:38 +0100

    SHINY: reactive event+plotly

--- my_app.py
+++ my_app.py
@@ -1,10 +1,13 @@
-from shiny import App, ui, render, reactive, Inputs, Outputs, Session
+from shiny import (App, ui, render, reactive, Inputs, Outputs, Session)
+from shinywidgets import output_widget, render_widget
 import shinyswatch
+import plotly.graph_objects as go
+import pandas as pd
 
 # import the wrapper objects for model interaction.
 from ciw_model import Experiment, multiple_replications
 
-app_ui = ui.page_fixed(
+app_ui = ui.page_fluid(
     shinyswatch.theme.minty(),
     ui.h2("Ciw Urgent Care Call Centre Model"),
     ui.markdown("""
@@ -27,14 +30,24 @@ app_ui = ui.page_fixed(
 
             # run simulation model button
             ui.input_action_button("run_sim", "Run Simulation", class_="btn-primary"),
+
+            width=2
         ),
         ui.panel_main(
-            ui.output_data_frame("result_table")
+
+            ui.row(
+                ui.column(5, ui.output_data_frame("result_table"),),
+                ui.column(7, output_widget("histogram")),
+            ),
+
         )
     )
 )
 
 def server(input: Inputs, output: Outputs, session: Session):
+
+    # reactive value for replication results.
+    replication_results = reactive.Value()
 
     def run_simulation():
         '''
@@ -64,20 +77,110 @@ def server(input: Inputs, output: Outputs, session: Session):
         -------
         pd.DataFrame
         '''
-        summary = replications.describe().round(2)
+        summary = replications.describe().round(2).T
         # resetting index because cannot figure out how to show index
-        return summary.reset_index() 
+        summary = summary.reset_index() 
+        summary = summary.rename(columns={'index': 'metric'})
+        return summary
+    
+    def create_user_filtered_hist(results):
+        '''
+        Create a plotly histogram that includes a drop down list that allows a user
+        to select which KPI is displayed as a histogram
+        
+        Params:
+        -------
+        results: pd.Dataframe
+            rows = replications, cols = KPIs
+            
+        Returns:
+        -------
+        plotly.figure
+        
+        Sources:
+        ------
+        The code in this function was partly adapted from two sources:
+        1. https://stackoverflow.com/questions/59406167/plotly-how-to-filter-a-pandas-dataframe-using-a-dropdown-menu
+        
+        Thanks and credit to `vestland` the author of the reponse.
+        
+        2. https://plotly.com/python/dropdowns/
+        '''
+        # create a figure
+        fig = go.Figure()
+
+        # set up a trace
+        fig.add_trace(go.Histogram(x=results[results.columns[0]]))
+
+        buttons = []
+
+        # create list of drop down items - KPIs
+        # the params in the code would need to vary depending on the type of chart.
+        # The histogram will show the first KPI by default
+        for col in results.columns:
+            buttons.append(dict(method='restyle',
+                                label=col,
+                                visible=True,
+                                args=[{'x':[results[col]],
+                                    'type':'histogram'}, [0]],
+                                )
+                        )
+
+        # create update menu and parameters
+        updatemenu = []
+        your_menu = dict()
+        updatemenu.append(your_menu)
+
+        updatemenu[0]['buttons'] = buttons
+        updatemenu[0]['direction'] = 'down'
+        updatemenu[0]['showactive'] = True
+        updatemenu[0]['x'] = 0.25
+        updatemenu[0]['y'] = 1.1
+        updatemenu[0]['xanchor'] = 'right'
+        updatemenu[0]['yanchor'] = 'bottom'
+             
+        # add dropdown menus to the figure
+        fig.update_layout(showlegend=False, 
+                        updatemenus=updatemenu)
+        
+        # add label for selecting performance measure
+        fig.update_layout(
+        annotations=[
+            dict(text="Performance measure", x=0, xref="paper", y=1.25, 
+                yref="paper", align="left", showarrow=False)
+        ])
+        return fig
 
     @output
     @render.data_frame
-    @reactive.event(input.run_sim)
     def result_table():
         '''
         Reactive event to when the run simulation button
         is clicked.
         '''
-        #await asyncio.sleep(2)
-        #with reactive.isolate():
-        return summary_results(run_simulation())
+        return summary_results(replication_results())
+    
+    @output
+    @render_widget
+    def histogram():
+        '''
+        Updates the interactive histogram
 
+        Returns:
+        -------
+        plotly.figure
+        '''
+        return create_user_filtered_hist(replication_results())
+    
+    @reactive.Effect
+    @reactive.event(input.run_sim)
+    def _():
+        '''
+        Runs simulation model when button is clicked.
+        This is a reactive effect. Once replication_results
+        is set it invalidates results_table and histogram.  
+        These are rerun by Shiny
+        '''
+        replication_results.set(run_simulation())
+        
 app = App(app_ui, server)
